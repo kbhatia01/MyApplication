855ef17b6ff6e430bfa8aaf6bd7eaa9126f14a56
karan <cooldownkb@gmail.com>
2018-06-05 16:37:29 +0530

    Add

--- src/pages/examination-details/examination-details.ts
+++ src/pages/examination-details/examination-details.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { EduserviceProvider } from '../../providers/eduservice/eduservice';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { File } from '@ionic-native/file';
@@ -23,59 +23,53 @@ declare var cordova: any;
 
 export class ExaminationDetailsPage implements OnInit {
   errorMsg: any;
-pk_datesheet_id:number;
-details=[];
-path: any;
+  pk_datesheet_id: number;
+  details = [];
+  path: any;
 
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private plaform:Platform,private file:File,private transfer:FileTransfer, private eduservice:EduserviceProvider,private loadingCtrl:LoadingController) {
-   this.pk_datesheet_id = navParams.get('pk_datesheet_id');
-   this.plaform.ready().then(() => {
-    // make sure this is on a device, not an emulation (e.g. chrome tools device mode)
-    if(!this.plaform.is('cordova')) {
-      return false;
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private plaform: Platform, private file: File, private transfer: FileTransfer, private eduservice: EduserviceProvider, private loadingCtrl: LoadingController) {
+    this.pk_datesheet_id = navParams.get('pk_datesheet_id');
+    this.plaform.ready().then(() => {
+      // make sure this is on a device, not an emulation (e.g. chrome tools device mode)
+      if (!this.plaform.is('cordova')) {
+        return false;
+      }
 
-    if (this.plaform.is('ios')) {
-      this.path = cordova.file.documentsDirectory;
-    }
-    else if(this.plaform.is('android')) {
-      this.path = cordova.file.dataDirectory;
-    }
-    else {
-      // exit otherwise, but you could add further types here e.g. Windows
-      return false;
-    }
-  });
+      if (this.plaform.is('ios')) {
+        this.path = cordova.file.documentsDirectory;
+      }
+      else if (this.plaform.is('android')) {
+        this.path = cordova.file.dataDirectory;
+      }
+      else {
+        // exit otherwise, but you could add further types here e.g. Windows
+        return false;
+      }
+    });
   }
 
-  ngOnInit(){
-   
+  ngOnInit() {
+
     this.eduservice.getDateSheetDetails(this.pk_datesheet_id)
-  .subscribe(result => {
-   console.log(result);
-    this.details=result.Item;
-  },
-    Error => this.errorMsg = Error
-  );
+      .subscribe(result => {
+        console.log(result);
+        this.details = result.Item;
+      },
+        Error => this.errorMsg = Error
+      );
 
   }
-  download(url:string){
-    let newurl=GlobalVariable.Downloadurl+url;
-
+  download(url: string) {
+    let newurl = GlobalVariable.Downloadurl + url;
+    console.log(newurl);
     this.plaform.ready().then(() => {
 
       const fileTransfer: FileTransferObject = this.transfer.create();
 
-      fileTransfer.download(newurl, this.path + "File.jpg").then((entry) => {
-
-      }), (error) => {
-
-        
-
-      }
+      fileTransfer.download(newurl, this.path + "File.jpg");
 
     });
-}
+  }
 }
